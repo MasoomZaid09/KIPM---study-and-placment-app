2510f79010701ac2239baace65963b1ad093aedc
Masoom <masoomzaid6392@gmail.com>
2021-11-24 16:41:37 +0530

    Add Navigation Drawer

--- app/src/main/java/com/example/kipmnotes/MainActivity.kt
+++ app/src/main/java/com/example/kipmnotes/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.kipmnotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.example.kipmnotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setUpToolbar()
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(this,binding.drawerLayout,R.string.open_drawer,R.string.close_drawer)
+        binding.drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
     }
+
+
+    fun setUpToolbar(){
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.title = "KIPM NOTES"
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == android.R.id.home){
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
