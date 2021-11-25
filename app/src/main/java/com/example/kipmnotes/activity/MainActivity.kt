ca20557088e5aff97b163fc7fdad7bd1d97d7ecc
Masoom <masoomzaid6392@gmail.com>
2021-11-25 14:22:01 +0530

    Added SignOutButton

--- app/src/main/java/com/example/kipmnotes/activity/MainActivity.kt
+++ app/src/main/java/com/example/kipmnotes/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kipmnotes.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
@@ -7,11 +8,12 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import com.example.kipmnotes.R
 import com.example.kipmnotes.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityMainBinding
-
+    private lateinit var mAuth:FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        mAuth = FirebaseAuth.getInstance()
 
 //      calling Toolbar function
         setUpToolbar()
@@ -106,6 +109,12 @@ class MainActivity : AppCompatActivity() {
                     binding.drawerLayout.closeDrawers()
                 }
 
+//              Adding Clicks on LogOut
+                R.id.logOut ->{
+                    mAuth.signOut()
+                    val intent = Intent(this,LoginActivity::class.java)
+                    startActivity(intent)
+                }
             }
             return@setNavigationItemSelectedListener true
         }
