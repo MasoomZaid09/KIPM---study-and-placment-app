76d36267786f290bf7600c5390a7d17a7f498455
Masoom <masoomzaid6392@gmail.com>
2021-11-30 12:14:32 +0530

    Added Contents in language Page

--- app/src/main/java/com/example/kipmnotes/fragment/LanguageFragment.kt
+++ app/src/main/java/com/example/kipmnotes/fragment/LanguageFragment.kt
@@ -1,11 +1,17 @@
 package com.example.kipmnotes.fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.kipmnotes.R
+import com.example.kipmnotes.adapter.LanguageRecyclerAdapter
+import com.example.kipmnotes.databinding.FragmentHomeBinding
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -13,8 +19,24 @@ private const val ARG_PARAM2 = "param2"
 
 class LanguageFragment : Fragment() {
 
+
     private var param1: String? = null
     private var param2: String? = null
+
+    lateinit var recyclerLanguage:RecyclerView
+    lateinit var layoutManager: RecyclerView.LayoutManager
+    lateinit var recyclerAdapter:LanguageRecyclerAdapter
+
+    private val languageList = arrayListOf<String>(
+        "  Learn Python",
+        "  Learn Java",
+        "  Learn C++",
+        "  Learn Javascript",
+        "  Learn C",
+        "  Learn SQL",
+        "  Learn R"
+        )
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +51,24 @@ class LanguageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val view = inflater.inflate(R.layout.fragment_language, container, false)
+        val view = inflater.inflate(R.layout.fragment_language,container,false)
+
+
+        recyclerLanguage = view.findViewById(R.id.recyclerLanguage)
+        layoutManager = LinearLayoutManager(activity)
+
+//        Added Divider b/w items
+        recyclerLanguage.addItemDecoration(
+            DividerItemDecoration(
+                recyclerLanguage.context,(layoutManager as LinearLayoutManager).orientation
+            )
+        )
+
+        recyclerAdapter = LanguageRecyclerAdapter(activity as Context,languageList)
+        recyclerLanguage.adapter = recyclerAdapter
+        recyclerLanguage.layoutManager = layoutManager
         return view
+
     }
 
     companion object {
