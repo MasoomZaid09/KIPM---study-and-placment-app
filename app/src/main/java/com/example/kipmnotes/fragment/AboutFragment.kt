24ff86af32cf2a29a6058160295ff2cf885cfb7c
Masoom <masoomzaid6392@gmail.com>
2021-12-03 14:37:45 +0530

    linked with Linkedin Profiles

--- app/src/main/java/com/example/kipmnotes/fragment/AboutFragment.kt
+++ app/src/main/java/com/example/kipmnotes/fragment/AboutFragment.kt
@@ -1,10 +1,13 @@
 package com.example.kipmnotes.fragment
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.kipmnotes.R
 
 private const val ARG_PARAM1 = "param1"
@@ -15,6 +18,10 @@ class AboutFragment : Fragment() {
 
     private var param1: String? = null
     private var param2: String? = null
+
+    lateinit var aboutMasoom:TextView
+    lateinit var aboutZakariya:TextView
+    lateinit var aboutVishal:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +35,20 @@ class AboutFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val view = inflater.inflate(R.layout.fragment_about,container,false)
+
+
+        aboutMasoom = view.findViewById(R.id.aboutMasoom)
+        aboutZakariya = view.findViewById(R.id.aboutZakariya)
+        aboutVishal = view.findViewById(R.id.aboutVishal)
+
+        // Linked with linkdin Profile
+        aboutMasoom.setMovementMethod(LinkMovementMethod.getInstance())
+        aboutMasoom.setLinkTextColor(Color.BLUE)
+        aboutZakariya.setMovementMethod(LinkMovementMethod.getInstance())
+        aboutZakariya.setLinkTextColor(Color.BLUE)
+        aboutVishal.setMovementMethod(LinkMovementMethod.getInstance())
+        aboutVishal.setLinkTextColor(Color.BLUE)
 
 
         return  view
